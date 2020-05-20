d130400559e1f7db78e80ae8293701b618aa84b5
et1911 <et1911@et.com>
2020-05-20 15:48:00 +0800

    add springmvc04-param

--- springmvc-04-param/src/main/java/com/etoak/bean/Student.java
+++ springmvc-04-param/src/main/java/com/etoak/bean/Student.java
@@ -1,33 +1,22 @@
 package com.etoak.bean;
 
+import java.util.List;
+import java.util.Map;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+//@Getter
+//@Setter
+//@ToString
+@Data  // get set toString
+@NoArgsConstructor
+@AllArgsConstructor
 public class Student {
 	private Integer id;
 	private String name;
 	private Integer age;
-	
-	
-	@Override
-	public String toString() {
-		return "Student [id=" + id + ", name=" + name + ", age=" + age + "]";
-	}
-	public Integer getId() {
-		return id;
-	}
-	public void setId(Integer id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public Integer getAge() {
-		return age;
-	}
-	public void setAge(Integer age) {
-		this.age = age;
-	}
-	
-	
+	private List<String> hobbyList;
+	private Map<String,Object> stuMap;
 }
